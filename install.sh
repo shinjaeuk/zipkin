0f7fcc75a0c8f566e33df9c108df505c608c4b72
재욱 신 <jushin@rockplace.co.kr>
2018-03-24 16:54:09 +0900

    rename docker hub

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #curl -sSL https://zipkin.io/quickstart.sh | bash -s
-docker build . -t hubtea/zipkin
-docker push hubtea/zipkin
+docker build . -t nockdookong/zipkin
+docker push nockdookong/zipkin
 kubectl apply -f deployment.yaml -n development
